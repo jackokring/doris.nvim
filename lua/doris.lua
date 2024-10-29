0e5a747ba3e9030e17a41be3fbcf82adb5b00205
jackokring <jackokring@gmail.com>
2024-10-29 21:11:40 +0000

    Tuesday 2024-10-29 21:11:40

--- lua/doris.lua
+++ lua/doris.lua
@@ -6,6 +6,9 @@ local module = require("doris.module")
 -- supply table of lines and opts
 local popup = require("plenary.popup").create
 
+-- short forms
+local a = vim.api
+
 ---@class Config
 ---@field opt string Your config option
 local config = {
@@ -42,11 +45,41 @@ M.hello = function()
 end
 
 -- impure function collection
-M.popup = function(what)
-  return popup(what, M.config.popup)
+---@param what table
+---@param inkey function(key: string)
+-- table of details and callbacks
+---@return table
+M.popup = function(what, inkey)
+  local win, xtra = popup(what, M.config.popup)
+  xtra.what = what
+  -- open new namespace for key listener
+  local keycb = function(key, typed)
+    -- just the keys
+    -- the translated keys for easy mapping cheats
+    if typed == "" then
+      xtra.close()
+      assert(false, "automated keys exit")
+    end
+    inkey(key)
+  end
+  xtra.keyns = vim.on_key(keycb, 0)
+  local buf = a.nvim_win_get_buf(win)
+  -- an on change callback function closure
+  xtra.show = function()
+    a.nvim_buf_set_lines(buf, 0, a.nvim_buf_line_count(buf) - 1, false, what)
+  end
+  -- a close callback for clean up
+  xtra.close = function()
+    -- close key listener
+    vim.on_key(nil, xtra.keyns)
+    a.nvim_win_close(win, true)
+    a.nvim_buf_delete(buf, { force = true })
+  end
+  return xtra
 end
 
 -- pure function import and pass export
+M.a = a
 M.bind = module.bind
 M.extends = module.extends
 
