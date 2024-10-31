bb4a9b070f7e28d15dc1ef5e7d608fc4cdb1008d
jackokring <jackokring@gmail.com>
2024-10-31 19:34:12 +0000

    Thursday 2024-10-31 19:34:12

--- lua/doris.lua
+++ lua/doris.lua
@@ -4,24 +4,26 @@
 -- this keeps the interface separate from the implementation
 -- of pure lua functions
 -- short forms for terse code coding, as contain many fields
-local d = require("doris.module")
+local dd = require("doris.module")
 -- async futures/promises
-local p = require("plenary.async")
+local as = require("plenary.async.async")
+local uv = require("plenary.async.uv_async")
+local ch = require("plenary.async.control")
 -- iterators
-local i = require("plenary.iterators")
+local it = require("plenary.iterators")
 -- class object with mixins via implements list
 -- not dynamic mixin binding
-local c = require("plenary.class")
+local cl = require("plenary.class")
 -- enums e { "x", ... }
-local e = require("plenary.enum")
+local en = require("plenary.enum")
 -- job control class
-local j = require("plenary.job")
+local jo = require("plenary.job")
 -- context manager
-local m = require("plenary.context_manager")
+local cm = require("plenary.context_manager")
 
 -- short forms
-local a = vim.api
 local f = vim.fn
+local a = vim.api
 
 ---@class Config
 ---@field doris table?
@@ -63,30 +65,41 @@ end
 -- impure function collection
 -- notify defaults
 M.notify = function(msg)
+  -- can't short name as used for title
   vim.notify(msg, vim.log.INFO, nil)
 end
 
 -- supply table of lines and opts
 local popup = require("plenary.popup").create
 
+local np = function() end
+
 ---@param what table
 ---@param inkey function(string)
 ---@param process function
+---@param reset function
 -- table of details and callbacks
 ---@return table
-M.popup = function(what, inkey, process)
+M.popup = function(what, inkey, process, reset)
   local win, xtra = popup(what, M.config.popup)
   xtra.what = what
   local buf = a.nvim_win_get_buf(win)
   -- add new key definitions for buffer
-  local keys = "abcdefghijklmnopqrstuvwxyz"
+  local keys = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_"
   local function nmap(key)
     vim.keymap.set("n", key, function()
       inkey(key)
     end, { buffer = buf })
   end
+  local function off(key, y)
+    return f.nr2char(f.char2nr(key, true) + y, true)
+  end
   for x = 1, #keys, 1 do
-    nmap(keys[x])
+    local y = keys[x]
+    nmap(y)
+    nmap("<C-" .. y .. ">")
+    nmap(off(y, 32))
+    nmap(off(y, -32))
   end
   -- specials
   vim.keymap.set("n", "<esc>", xtra.close, {
@@ -95,6 +108,7 @@ M.popup = function(what, inkey, process)
   -- must follow this for to be defined for "recursive call"
   -- 10 fps
   xtra.run = true
+  reset()
   local function do_proces()
     if not xtra.run then
       return
@@ -121,29 +135,33 @@ end
 M.test_popup = function()
   M.popup({ "test", "line 2" }, function(key)
     M.notify(key)
-  end, function() end)
+  end, np, np)
 end
 
 -- function import and pass export
--- api
-M.a = a
--- fn
-M.f = f
+-- from doris module
+-- only pure functions not needing vim calls
+M.dd = dd
+-- vim.fn
+-- might be extended
+M.fn = f
+M.np = np
 -- then from plenary modules
 -- promises/futures async
-M.p = p
+M.as = as
+-- file ops
+M.uv = uv
+-- control channels
+M.ch = ch
 -- iterators
-M.i = i
+M.it = it
 -- classes
-M.c = c
+M.cl = cl
 -- enums (capitalized string table)
-M.e = e
+M.en = en
 -- job control class
-M.j = j
+M.jo = jo
 -- context manager (like python file on each etc.)
-M.m = m
--- then from doris module
--- only pure functions not needing vim calls
-M.d = d
+M.cm = cm
 
 return M
