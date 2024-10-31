855b39fd5ace052670388f7b762395886c9e6b39
jackokring <jackokring@gmail.com>
2024-10-31 17:16:59 +0000

    Thursday 2024-10-31 17:16:59

--- lua/doris.lua
+++ lua/doris.lua
@@ -70,41 +70,47 @@ end
 local popup = require("plenary.popup").create
 
 ---@param what table
----@param inkey function(key: string, mod: integer)
+---@param inkey function(string)
+---@param process function
 -- table of details and callbacks
 ---@return table
-M.popup = function(what, inkey)
+M.popup = function(what, inkey, process)
   local win, xtra = popup(what, M.config.popup)
   xtra.what = what
-  -- open new namespace for key listener
-  local keycb = function(key, typed)
-    local k = f.getcharstr()
-    -- check ESC or unassigned ALT
-    if f.char2nr(k) == 27 then
-      -- escape exit and dual on alt call solved?
-      xtra.close()
-      -- then wait for an escape key
-      while f.getchar() ~= 27 do
-        M.notify("Press <esc> again.")
-      end
-    end
-    -- will this technically consume a key event?
-    -- 2 = shift
-    -- 4 = control
-    -- 8 = alt
-    -- 128 = super
-    inkey(k, f.getcharmod())
-  end
-  xtra.keyns = vim.on_key(keycb, 0)
   local buf = a.nvim_win_get_buf(win)
+  -- add new key definitions for buffer
+  local keys = "abcdefghijklmnopqrstuvwxyz"
+  local function nmap(key)
+    vim.keymap.set("n", key, function()
+      inkey(key)
+    end, { buffer = buf })
+  end
+  for x = 1, #keys, 1 do
+    nmap(keys[x])
+  end
+  -- specials
+  vim.keymap.set("n", "<esc>", xtra.close, {
+    buffer = buf,
+  })
+  -- must follow this for to be defined for "recursive call"
+  -- 10 fps
+  xtra.run = true
+  local function do_proces()
+    if not xtra.run then
+      return
+    end
+    pcall(process)
+    vim.defer_fn(do_proces, 100)
+  end
+  vim.defer_fn(do_proces, 100)
   -- an on change callback function closure
   xtra.show = function()
     a.nvim_buf_set_lines(buf, 0, a.nvim_buf_line_count(buf) - 1, false, what)
   end
   -- a close callback for clean up
   xtra.close = function()
-    -- close key listener
-    vim.on_key(nil, xtra.keyns)
+    -- close run
+    xtra.run = false
     a.nvim_win_close(win, true)
     a.nvim_buf_delete(buf, { force = true })
   end
@@ -113,9 +119,9 @@ end
 
 -- test the popup by showing notify of key events recieved
 M.test_popup = function()
-  M.popup({ "test", "line 2" }, function(key, mod)
+  M.popup({ "test", "line 2" }, function(key)
     M.notify(key)
-  end)
+  end, function() end)
 end
 
 -- function import and pass export
