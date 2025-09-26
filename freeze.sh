d8c012724dc5b82faba09733ab5e3da402741ca6
jackokring <jackokring@gmail.com>
2025-09-26 12:29:11 +0000

    Tidy

--- freeze.sh
+++ freeze.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/bash
 . xdg.sh
 pip freeze | tee requirements.txt
-# copy some user file backups
-pushd extras-backup || exit
-# $XDG
-cp -r "$XDG_CONFIG_HOME/nano" .
-cp -r "$XDG_CONFIG_HOME/rofi" .
-cp -r "$XDG_CONFIG_HOME/neofetch" .
-cp -r "$XDG_CONFIG_HOME/nvim" .
-# irregular
-cp -r ~/.local/share/fonts/JetBrainsMono .
-cp ~/.bashrc .
-cp "$XDG_CONFIG_HOME/starship.toml" .
-cp -r ~/.tmux .
-cp ~/.tmux.conf .
-popd || exit
 #rm -rf "$(find extras-backup -type d -name .git)"
 # add commit push
 gacp() {
